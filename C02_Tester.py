9311fd065d78ae285852aa680511d40d71140ef8
Francisco Domingues <fsoares-@student.42barcelona.com>
2021-12-15 19:43:57 +0100

    Aded tests to c02

--- C02_Tester.py
+++ C02_Tester.py
@@ -23,3 +23,26 @@ class C02_Tester(CommonTester):
         self.exercise_files = ["ft_str_is_lowercase.c"]
         self.test_files = ["main.c"]
 
+    def ex05(self):
+        self.exercise_files = ["ft_str_is_uppercase.c"]
+        self.test_files = ["main.c"]
+
+    def ex06(self):
+        self.exercise_files = ["ft_str_is_printable.c"]
+        self.test_files = ["main.c"]
+
+    def ex07(self):
+        self.exercise_files = ["ft_strupcase.c"]
+        self.test_files = ["main.c"]
+
+    def ex08(self):
+        self.exercise_files = ["ft_strlowcase.c"]
+        self.test_files = ["main.c"]
+
+    def ex09(self):
+        self.exercise_files = ["ft_strcapitalize.c"]
+        self.test_files = ["main.c"]
+
+    def ex10(self):
+        self.exercise_files = ["ft_strlcpy.c"]
+        self.test_files = ["main.c"]
