c95415581900dda61bb88618d5bb4dacd8e42258
Francisco Domingues <fsoares-@student.42barcelona.com>
2021-12-20 22:22:35 +0100

    Added remaining mains for c04

--- mains/c04/ex03/main.c
+++ mains/c04/ex03/main.c
@@ -6,7 +6,7 @@
 /*   By: fsoares- <fsoares-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 14:45:28 by fsoares-          #+#    #+#             */
-/*   Updated: 2021/12/20 17:55:53 by fsoares-         ###   ########.fr       */
+/*   Updated: 2021/12/20 22:12:14 by fsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int	ft_atoi(char *str);
 int main() {
 	printf("%i\n", ft_atoi("\t\n\v\f\r      23"));
 	printf("%i\n", ft_atoi("     ------+++++---12341"));
+	printf("%i\n", ft_atoi("     ------+++++---123413"));
 	printf("%i\n", ft_atoi("00000000002147483647"));
 	printf("%i\n", ft_atoi("-2147483648"));
 	printf("%i\n", ft_atoi("a"));
--- mains/c04/ex05/main.c
+++ mains/c04/ex05/main.c
@@ -6,23 +6,29 @@
 /*   By: fsoares- <fsoares-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 20:06:11 by fsoares-          #+#    #+#             */
-/*   Updated: 2021/12/20 20:59:04 by fsoares-         ###   ########.fr       */
+/*   Updated: 2021/12/20 22:21:29 by fsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <limits.h>
 
 int	ft_atoi_base(char *nbr, char *base);
 
 int		main(void)
 {
+	int res;
 	printf("42:%d\n", ft_atoi_base("2a", "0123456789abcdef"));
 	printf("-42:%d\n", ft_atoi_base("   --------+-2a", "0123456789abcdef"));
 	printf("42:%d\n", ft_atoi_base("   -+-2a", "0123456789abcdef"));
 	printf("0:%d\n", ft_atoi_base("   --------+- 2a", "0123456789abcdef"));
-	printf("0:%d\n", ft_atoi_base("   --------+-z", "0123456789abcdef"));
+	printf("0:%d\n", ft_atoi_base("   --------+-g", "0123456789abcdef"));
 	printf("0:%d\n", ft_atoi_base("   --------+-2a", ""));
 	printf("0:%d\n", ft_atoi_base("   --------+-2a", "0"));
 	printf("0:%d\n", ft_atoi_base("   --------+-2a", "+-0"));
 	printf("0:%d\n", ft_atoi_base("   --------+-2a", "\t01"));
+	res = ft_atoi_base(" \v7fffffff", "0123456789abcdef");
+	printf("%d : %d (equals: %i)\n", INT_MAX, res, INT_MAX == res);
+	res = ft_atoi_base(" \f-80000000", "0123456789abcdef");
+	printf("%d : %d (equals: %i)\n", INT_MIN, res, INT_MIN == res);
 }
