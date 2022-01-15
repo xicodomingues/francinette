45a102205f5ead939b61634f0b0c6c330701e338
Francisco Domingues <fsoares-@student.42barcelona.com>
2022-01-15 17:31:00 +0100

    separated my tests

--- testers/libft/ExecuteFsoares.py
+++ testers/libft/ExecuteFsoares.py
@@ -1,11 +1,10 @@
-
-from imp import init_frozen
 import logging
 import os
 import subprocess
 from typing import List
 from main import CT
 from testers.libft.BaseExecutor import remove_ansi_colors
+from halo import Halo
 
 logger = logging.getLogger()
 
@@ -28,15 +27,22 @@ class ExecuteFsoares():
 		os.chdir(self.temp_dir)
 		logger.info(f"On directory {os.getcwd()}")
 
-		print(f"\n{CT.CYAN}Compiling tests from: {CT.WHITE}{self.folder}{CT.NC}", end="")
-		for func in self.to_execute:
-			subprocess.run(f"gcc -Wall -Wextra utils.c test_{func}.c -L. -lft", shell=True)
+		text = f"{CT.CYAN}Compiling tests from: {CT.WHITE}{self.folder}{CT.NC}"
+		with Halo(text=text) as spinner:
+			for func in self.to_execute:
+				command = f"gcc -Wall -Wextra utils.c test_{func}.c -L. -lft -o test_{func}.out"
+				logger.info(f"Executing: {command}")
+				res = subprocess.run(command, shell=True, capture_output=True, text=True)
+				if res.returncode != 0:
+					spinner.fail()
+					print(res.stderr)
+					raise Exception("Problem compiling the tests")
 
 	def execute_tests(self):
-		execute = ["./a.out"]
-		print(f"\n{CT.CYAN}Executing: {CT.WHITE}{' '.join(execute)}{CT.NC}:")
+		print(f"\n{CT.CYAN}Executing Tests: {CT.NC}:")
 
-		p = subprocess.run(execute, capture_output=True, text=True)
-		print(p.stdout, CT.NC)
+		for func in self.to_execute:
+			p = subprocess.run(f"test_{func}.out", capture_output=True, text=True)
+			print(p.stdout, CT.NC)
 
 		return [remove_ansi_colors(line) for line in p.stdout.splitlines()]
