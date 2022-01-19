88615f1741334bb5e672930f4f768872d250d9bb
Francisco Domingues <fsoares-@student.42barcelona.com>
2022-01-19 18:46:36 +0100

    added strmapi test

--- mains/libft/fsoares/test_strmapi.c
+++ mains/libft/fsoares/test_strmapi.c
@@ -1,8 +1,32 @@
 
 #include "utils.h"
 
+int single_test_strmapi(char *str, char(*fn)(unsigned int, char), char *func, char *expected)
+{
+	set_sign("ft_strmapi(\"%s\", %s)", str, func);
+	check_alloc_str_return(ft_strmapi(str, fn), expected);
+}
+
+char add(unsigned int i, char c) {
+	return (char)(i + c);
+}
+
+char to_char_zero(unsigned int i, char c) {
+	c = (char)i;
+	return '0';
+}
+
+int test_strmapi()
+{
+	int res = single_test_strmapi("", add, "{(i, c) => i + c}", "");
+	res = single_test_strmapi("abcd0 ", add, "{(i, c) => i + c}", "aceg4%") && res;
+	res = single_test_strmapi("abcdfsdfs", to_char_zero, "{(i, c) => i + c}", "000000000") && res;
+
+	return res;
+}
+
 int	main()
 {
 	handle_signals();
-	no_test(strmapi);
+	test(strmapi);
 }
