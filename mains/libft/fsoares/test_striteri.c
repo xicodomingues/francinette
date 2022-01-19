773140c436065caace97adf2822cef362e7aeab5
Francisco Domingues <fsoares-@student.42barcelona.com>
2022-01-19 18:59:18 +0100

    added striteri tests

--- mains/libft/fsoares/test_striteri.c
+++ mains/libft/fsoares/test_striteri.c
@@ -1,8 +1,39 @@
 
 #include "utils.h"
 
+int single_test_striteri(char *str, void (*fn)(unsigned int, char *), char *func, char *expected)
+{
+	set_sign("ft_striteri(\"%s\", %s)", str, func);
+
+	char buffer[20];
+	strcpy(buffer, str);
+	int result = 1;
+	ft_striteri(buffer, fn);
+	result = same_string(expected, buffer);
+	result = check_leaks(NULL) && result;
+	null_check(char *, ft_striteri(buffer, fn), result);
+	return result;
+}
+
+void add(unsigned int i, char *c) {
+	*c = (char)(i + *c);
+}
+
+void to_char_zero(unsigned int i, char *c) {
+	*c = i - i + '0';
+}
+
+int test_striteri()
+{
+	int res = single_test_striteri("", add, "{(i, c) => i + c}", "");
+	res = single_test_striteri("abcd0 ", add, "{(i, c) => i + c}", "aceg4%") && res;
+	res = single_test_striteri("abcdfsdfs", to_char_zero, "{(i, c) => '0'}", "000000000") && res;
+
+	return res;
+}
+
 int	main()
 {
 	handle_signals();
-	no_test(striteri);
+	test(striteri);
 }
