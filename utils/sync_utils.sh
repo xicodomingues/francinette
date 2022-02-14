2c69ef59104922660a43b2d48b80431c0d4167fb
Francisco Domingues <fsoares-@student.42barcelona.com>
2022-02-14 21:15:48 +0100

    imrpoved sync utils

--- utils/sync_utils.sh
+++ utils/sync_utils.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-base=$(dirname "$SCRIPT_DIR")/tests
+base=$(dirname "$SCRIPT_DIR")
 
-cp -r "$base"/utils "$base"/get_next_line/fsoares
-cp -r "$base"/utils "$base"/printf/fsoares
-cp -r "$base"/utils "$base"/libft/fsoares
+cd $base || exit
+
+if [[ $1 == "-f" ]]; then
+	force=1
+fi
+
+update_utils()
+{
+	DIR=$1
+	changes=$(git diff --quiet $DIR/utils || printf "changed" )
+	if [[ $changes == "changed" && $force -ne 1 ]]; then
+		echo "$DIR has changes!"
+	else
+		cp -r "$base"/tests/utils $DIR
+	fi
+}
+
+update_utils tests/libft/fsoares
+update_utils tests/get_next_line/fsoares
+update_utils tests/printf/fsoares
