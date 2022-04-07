06a642890aacacf769e12a24dd45a7409c314a8d
Francisco Domingues <fsoares-@student.42barcelona.com>
2022-04-07 16:10:04 +0200

    Increment version

--- tester.sh
+++ tester.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 current_dir=$PWD
 
 # check for updates
-version=67
+version=68
 
 cd "$current_dir" || exit
 source "$DIR"/venv/bin/activate
