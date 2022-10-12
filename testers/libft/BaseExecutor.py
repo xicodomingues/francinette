c00309d850ef3d32b60708364aa27ba72a66de3e
Francisco Domingues <fsoares-@student.42barcelona.com>
2022-10-12 18:11:24 +0200

    Adapt changes to the code

--- testers/libft/BaseExecutor.py
+++ testers/libft/BaseExecutor.py
@@ -8,57 +8,20 @@ ansi_columns = re.compile(r'\x1B(?:\[[0-?]*G)')
 ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
 
 PART_1_FUNCTIONS = [
-    "atoi",
-    "bzero",
-    "calloc",
-    "isalnum",
-    "isalpha",
-    "isascii",
-    "isdigit",
-    "isprint",
-    "memchr",
-    "memcmp",
-    "memcpy",
-    "memmove",
-    "memset",
-    "strchr",
+    "isalpha", "isdigit", "isalnum", "isascii", "isprint", "strlen", "memset", "bzero", "memcpy", "memmove", "strlcpy",
+    "strlcat", "toupper", "tolower", "strchr", "strrchr", "strncmp", "memchr", "memcmp", "strnstr", "atoi", "calloc",
     "strdup"
-    "strlcat",
-    "strlcpy",
-    "strlen",
-    "strncmp",
-    "strnstr",
-    "strrchr",
-    "tolower",
-    "toupper",
 ]
 
 PART_2_FUNCTIONS = [
-    "itoa",
-    "putchar_fd",
-    "putendl_fd",
+    "substr", "strjoin", "strtrim", "split", "itoa", "strmapi", "striteri", "putchar_fd", "putstr_fd", "putendl_fd",
     "putnbr_fd"
-    "putstr_fd",
-    "split",
-    "striteri",
-    "strjoin",
-    "strmapi",
-    "strtrim",
-    "substr",
 ]
 
 BONUS_FUNCTIONS = [
-    "lstadd_back",
-    "lstadd_front",
-    "lstclear",
-    "lstdelone",
-    "lstiter",
-    "lstlast",
-    "lstmap"
-    "lstnew",
-    "lstsize",
+    "lstnew", "lstadd_front", "lstsize", "lstlast", "lstadd_back", "lstdelone", "lstclear", "lstiter", "lstmap"
 ]
 
 
 def remove_ansi_colors(text):
-    return ansi_escape.sub('', ansi_columns.sub(' ', text))
+	return ansi_escape.sub('', ansi_columns.sub(' ', text))
