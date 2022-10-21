cdc9920f11dbb0b7e4cf9abea3c6f6b62ae0d997
Francisco Domingues <fsoares-@student.42barcelona.com>
2022-10-21 16:24:59 +0200

    fix(gnl): Allow reads of size zero

--- tests/get_next_line/fsoares/file_utils.c
+++ tests/get_next_line/fsoares/file_utils.c
@@ -5,7 +5,7 @@ ssize_t read(int fildes, void *buf, size_t nbyte)
 {
 	ssize_t(*libc_read)(int, void *, size_t) =
 		(ssize_t (*)(int, void *, size_t))dlsym(RTLD_NEXT, "read");
-	if (nbyte != BUFFER_SIZE) {
+	if (nbyte != BUFFER_SIZE && nbyte != 0) {
 		fprintf(errors_file, "Please use the read size provided by the compile variable BUFFER_SIZE: %i\n", BUFFER_SIZE);
 		printf(RED "Use the fucking BUFFER_SIZE (%i), instead of what you wanted to read (%zi)...\n" NC, BUFFER_SIZE, nbyte);
 		exit(1);
