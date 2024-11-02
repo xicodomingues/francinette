155c92ae956ddae4fb10ebaf6ad34c81c4f333e0
murmur <0aeon0@mail.com>
2024-11-02 06:28:51 +0300

    fix pass param with alias

--- bin/dockerize/set-alias.bash
+++ bin/dockerize/set-alias.bash
@@ -1,4 +1,4 @@
-line="alias murminette='curl https://raw.githubusercontent.com/murmurlab/francinette/refs/heads/murmurlab-docker/bin/dockerize/Dockerfile | docker build -t murminette - && docker run -t --rm -v .:/tmp/proj murminette'"
+line="alias murminette='curl https://raw.githubusercontent.com/murmurlab/francinette/refs/heads/murmurlab-docker/bin/dockerize/Dockerfile | docker build -t murminette - && docker run -t --rm -v .:/tmp/proj murminette murminette'"
 # line="alias murminette='docker run --rm -v .:/tmp/proj \`curl https://raw.githubusercontent.com/murmurlab/francinette/refs/heads/murmurlab-docker/bin/dockerize/Dockerfile | docker build -q -\`'"
 >>~/.bash_profile cat <<END
 $line
