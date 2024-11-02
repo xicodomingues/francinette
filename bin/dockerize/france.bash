d60ac782f304d5fa94c825f31e17a050e5be7607
murmur <0aeon0@mail.com>
2024-11-02 06:56:13 +0300

    move bash file to inline bash in dockerfile.

--- bin/dockerize/france.bash
+++
@@ -1,7 +0,0 @@
-python3 -m venv /root/.venv
-source /root/.venv/bin/activate
-python3 -m pip install --upgrade pip setuptools
-python3 -m pip install norminette
-echo "export PATH=\$PATH:/root/.venv/bin" >> /root/.zshrc
-bash -c "$(curl -fsSL https://raw.github.com/xicodomingues/francinette/master/bin/install.sh)"
-deactivate
